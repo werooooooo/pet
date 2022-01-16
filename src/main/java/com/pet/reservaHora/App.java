94ea25dd6a304f5dc0ae81cb2d5c9f8a598132f7
pibaceta <ibaceta.sasso@gmail.com>
2022-01-16 14:27:49 -0300

    primero de examen

---
+++ src/main/java/com/pet/reservaHora/App.java
@@ -0,0 +1,13 @@
+package com.pet.reservaHora;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
